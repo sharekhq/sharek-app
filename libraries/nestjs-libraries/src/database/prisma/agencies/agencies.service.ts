cd41d4a840758fbb043a20a52ac95ee434e9bc31
moatazkhalifa <moataz@gmx.com>
2026-07-17 16:06:31 +0300

    fix: strip postiz.com links and founder recipient from dormant agency emails

--- libraries/nestjs-libraries/src/database/prisma/agencies/agencies.service.ts
+++ libraries/nestjs-libraries/src/database/prisma/agencies/agencies.service.ts
@@ -50,7 +50,7 @@ export class AgenciesService {
 <body style="font-family: Arial, sans-serif; margin: 0; padding: 0;">
   Hi there, <br /><br />
   Your agency ${agency?.name} has been added to Sharek!<br />
-  You can <a href="https://postiz.com/agencies/${agency?.slug}">check it here</a><br />
+  You can <a href="${process.env.FRONTEND_URL}/agencies/${agency?.slug}">check it here</a><br />
   It will appear on the main agency of Sharek in the next 24 hours.<br /><br />
 </body>
 </html>`
@@ -85,7 +85,7 @@ export class AgenciesService {
   async createAgency(user: User, body: CreateAgencyDto) {
     const agency = await this._agenciesRepository.createAgency(user, body);
     await this._notificationService.sendEmail(
-      'nevo@postiz.com',
+      process.env.EMAIL_FROM_ADDRESS!,
       'New agency created',
       `
 <html lang="en">
@@ -193,10 +193,10 @@ export class AgenciesService {
         </tr>
         <tr>
             <td style="padding: 20px; text-align: center; background-color: #000;">
-                <a href="https://postiz.com/agencies/action/approve/${
+                <a href="${process.env.FRONTEND_URL}/agencies/action/approve/${
                   agency.id
                 }" style="margin: 0 10px; text-decoration: none; color: #007bff;">To approve click here</a><br /><br /><br />
-                <a href="https://postiz.com/agencies/action/decline/${
+                <a href="${process.env.FRONTEND_URL}/agencies/action/decline/${
                   agency.id
                 }" style="margin: 0 10px; text-decoration: none; color: #007bff;">To decline click here</a><br /><br /><br />
             </td>
